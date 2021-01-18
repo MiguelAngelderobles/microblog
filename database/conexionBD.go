2e379e6b6bd070eaed9756378b028e88d9068853
MiguelAngelderobles <miguel.angelderobles@hotmail.com>
2021-01-18 10:24:40 -0300

    change in database conexion

--- database/conexionBD.go
+++ database/conexionBD.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/MiguelAngelderobles/microblog/models"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
