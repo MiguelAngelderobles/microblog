419260e7f4c03b740ed36e97193c6125cc3d6898
MiguelAngelderobles <miguel.angelderobles@hotmail.com>
2021-01-19 14:51:47 -0300

    change name bd and jwt add respuesta login and login routers

---
+++ bd/conexionBD.go
@@ -0,0 +1,57 @@
+package bd
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/MiguelAngelderobles/microblog/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+/*MongoCN es el objeto de conexión a la base de datos*/
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://usuario_1:SkillFactory2021@cluster0.5k8zj.mongodb.net/microblog?retryWrites=true&w=majority")
+
+/*ConectarBD es la función que me permite conectar a la base de datos
+  Devuelve una conexión a la BD del tipo Mongo Client*/
+func ConectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexión exitosa con la BD")
+	return client
+}
+
+/*ChequeoConnection es el ping a la BD*/
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
+
+func InsertoRegistro(u models.Usuario) (string, bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("microblog")
+	col := database.Collection("usuarios")
+	u.Password, _ = EncriptarPassword(u.Password)
+	result, err := col.InsertOne(ctx, u)
+	if err != nil {
+		return "", false, err
+	}
+	ObjID, _ := result.InsertedID.(primitive.ObjectID)
+	return ObjID.String(), true, nil
+}
